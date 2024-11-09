842cf88b043dbc46798e56cf6bf856edfd36588a
BoD <BoD@JRAF.org>
2024-11-09 16:30:04 +0100

    Chat in ContactScreen: initial support

--- src/main/kotlin/org/jraf/miniteljraf/main/minitel/contact/ContactScreen.kt
+++ src/main/kotlin/org/jraf/miniteljraf/main/minitel/contact/ContactScreen.kt
@@ -29,25 +29,38 @@ import org.jraf.klibminitel.core.CharacterSize
 import org.jraf.klibminitel.core.FunctionKey
 import org.jraf.klibminitel.core.Minitel
 import org.jraf.klibminitel.core.SCREEN_HEIGHT_NORMAL
+import org.jraf.klibminitel.core.SCREEN_WIDTH_NORMAL
 import org.jraf.miniteljraf.main.minitel.ParameterlessJrafScreen
 import org.jraf.miniteljraf.main.minitel.Resources
 import org.jraf.miniteljraf.main.minitel.app.MinitelApp
+import org.jraf.miniteljraf.util.Line
+import org.jraf.miniteljraf.util.printCentered
+import org.jraf.miniteljraf.util.wrapped
+
+private const val INPUT_HEIGHT = 3
+private const val FOOTER_HEIGHT = 2
+private val LOGO_AND_INTRO_HEIGHT = Resources.logoHeight + 4
 
 class ContactScreen(
   context: MinitelApp.Context,
   connection: Minitel.Connection,
   private val onBack: suspend () -> Unit,
 ) : ParameterlessJrafScreen(context, connection) {
+  private var input = ""
+  private val messages = mutableListOf<Message>()
+
   override suspend fun start(startParameters: Unit) {
     connection.screen.drawScreen()
   }
 
   private suspend fun Minitel.Screen.drawScreen() {
-    drawText()
+    drawIntro()
     drawFooter()
+    drawInputWindow()
+    drawInput()
   }
 
-  private suspend fun Minitel.Screen.drawText() {
+  private suspend fun Minitel.Screen.drawIntro() {
     moveCursor(0, Resources.logoHeight)
     clearBottomOfScreen()
     moveCursor(3, Resources.logoHeight + 2)
@@ -57,44 +70,162 @@ class ContactScreen(
     underline(true)
     characterSize(CharacterSize.TALL)
     print(" BoD@JRAF.org")
+    characterSize(CharacterSize.NORMAL)
+    colorForeground(4)
+    print('\n')
+    printCentered("or leave me a message below!")
   }
-
-  // TODO Implement a text area to send an email to BoD@JRAF
 
   private suspend fun Minitel.Screen.drawFooter() {
     moveCursor(0, SCREEN_HEIGHT_NORMAL - 2)
-    colorForeground(5)
-    colorBackground(1)
-    print(" Press")
-    underline(true)
-    print(" ")
-    inverse(true)
-    print(" Sommaire ")
-    underline(false)
-    inverse(false)
-    print(" or")
+    color(1, 5)
     underline(true)
     print(" ")
     inverse(true)
     print(" Retour ")
     underline(false)
     inverse(false)
-    print(" to go")
+    print(" go back to the home screen")
     clearEndOfLine()
 
     moveCursor(0, SCREEN_HEIGHT_NORMAL - 1)
-    colorForeground(5)
-    colorBackground(1)
-    print(" back to the home screen.")
+    color(1, 5)
+    underline(true)
+    print(" ")
+    inverse(true)
+    print(" Envoi ")
+    underline(false)
+    inverse(false)
+    print(" send your message")
     clearEndOfLine()
   }
 
+  private suspend fun Minitel.Screen.drawInputWindow() {
+    moveCursor(0, SCREEN_HEIGHT_NORMAL - INPUT_HEIGHT - FOOTER_HEIGHT)
+    inverse(true)
+    color(background0To7 = 0, foreground0To7 = 3)
+    clearEndOfLine()
+
+    moveCursorDown()
+    clearEndOfLine()
+
+    moveCursorDown()
+    clearEndOfLine()
+  }
+
+  private suspend fun Minitel.Screen.drawInput() {
+    moveCursor(0, SCREEN_HEIGHT_NORMAL - INPUT_HEIGHT - FOOTER_HEIGHT)
+    inverse(true)
+    color(background0To7 = 0, foreground0To7 = 3)
+    print(input)
+    updateCursor()
+  }
+
+  private suspend fun Minitel.Screen.updateCursor() {
+    showCursor(input.length < SCREEN_WIDTH_NORMAL * INPUT_HEIGHT)
+  }
 
   override suspend fun onKeyboard(e: Minitel.KeyboardEvent) {
-    if (e !is Minitel.KeyboardEvent.FunctionKeyEvent) return
-    when (e.functionKey) {
-      FunctionKey.SOMMAIRE, FunctionKey.RETOUR -> onBack()
-      else -> {}
+    when (e) {
+      is Minitel.KeyboardEvent.CharacterEvent -> {
+        if (e.char.isISOControl()) return
+        if (input.length >= SCREEN_WIDTH_NORMAL * INPUT_HEIGHT) {
+          connection.screen.beep()
+          return
+        }
+        val c = e.char.invertCase()
+        input += c
+        connection.screen.print(c)
+
+        connection.screen.updateCursor()
+      }
+
+      is Minitel.KeyboardEvent.FunctionKeyEvent -> {
+        when (e.functionKey) {
+          FunctionKey.SOMMAIRE, FunctionKey.RETOUR -> onBack()
+          FunctionKey.CORRECTION -> {
+            if (input.isNotEmpty()) {
+              input = input.dropLast(1)
+              connection.screen.moveCursorLeft()
+              connection.screen.clearEndOfLine()
+
+              connection.screen.updateCursor()
+            }
+          }
+
+          FunctionKey.ANNULATION -> {
+            input = ""
+            connection.screen.drawInputWindow()
+            connection.screen.drawInput()
+            connection.screen.updateCursor()
+          }
+
+          FunctionKey.ENVOI -> {
+            connection.screen.handleInput()
+          }
+
+          else -> {}
+        }
+      }
     }
+  }
+
+  private var bufferCursor = 0
+
+  private suspend fun Minitel.Screen.drawMessages() {
+    showCursor(false)
+    val buffer = messages.toLines()
+    val bufferHeight = SCREEN_HEIGHT_NORMAL - LOGO_AND_INTRO_HEIGHT - INPUT_HEIGHT - FOOTER_HEIGHT
+    val bufferWindow = buffer.takeLast(bufferHeight)
+    if (bufferWindow.size < bufferHeight) {
+      val currentBufferCursor = bufferCursor
+      for (i in bufferWindow.indices) {
+        if (i >= currentBufferCursor) {
+          moveCursor(0, i + LOGO_AND_INTRO_HEIGHT)
+          clearEndOfLine()
+          val line = bufferWindow[i]
+          colorForeground(line.foregroundColor)
+          print(line.text)
+          bufferCursor++
+        }
+      }
+    } else {
+      for (i in bufferWindow.indices.reversed()) {
+        moveCursor(0, i + LOGO_AND_INTRO_HEIGHT)
+        clearEndOfLine()
+        val line = bufferWindow[i]
+        colorForeground(line.foregroundColor)
+        print(line.text)
+      }
+    }
+  }
+
+  private suspend fun Minitel.Screen.handleInput() {
+    val input = input
+    this@ContactScreen.input = ""
+    showCursor(false)
+    drawInputWindow()
+    messages += Message.Local(input)
+    messages += Message.Remote("Thank you for your message!", "BoD")
+    drawMessages()
+    drawInput()
+  }
+
+  private fun Char.invertCase(): Char {
+    return if (isLowerCase()) uppercaseChar() else lowercaseChar()
+  }
+
+  private sealed interface Message {
+    class Local(val text: String) : Message
+    class Remote(val text: String, author: String) : Message
+  }
+
+  private fun List<Message>.toLines(): List<Line> {
+    return map {
+      when (it) {
+        is Message.Local -> Line(text = it.text, characterSize = CharacterSize.NORMAL, foregroundColor = 5)
+        is Message.Remote -> Line(text = it.text, characterSize = CharacterSize.NORMAL, foregroundColor = 7)
+      }
+    }.flatMap { it.wrapped(SCREEN_WIDTH_NORMAL) }
   }
 }
